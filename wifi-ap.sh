43e17d5a3afc1fa1d696ffaee86b149606a499d8
Blennes77 <Blennes77@users.noreply.github.com>
2018-04-18 09:13:45 +0200

    Update wifi-ap.sh

--- wifi-ap.sh
+++ wifi-ap.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-WIFI_DEV=wlp2s0
-ETH_DEV=enp0s25
+WIFI_DEV=wlp58s0
+ETH_DEV=eno1
 
 sudo yum -y install haveged iw hostapd
 if [ ! -d create_ap ]
 then
-	sudo git clone https://github.com/oblique/create_ap
+	sudo git clone https://github.com/Blennes77/create_ap
 fi
 
 cd create_ap; sudo make install
